6bb543c09832e8c4f8f970f7bb30d8ae5800c9b0
Melissa Astbury <melissaastbury@hotmail.com>
2021-03-23 11:57:00 +0000

    defined an object and made an object

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Mel", 100);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with the balance of {account.Balance}");
+        }
+    }
+}
---
+++ MySuperBank/bin/Debug/netcoreapp3.1/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        // Define what a bank account should look like
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+
+
+
+        // You need to make a bank account - this is called the constructor
+        public BankAccount(string name, decimal initalBalance)
+        {
+            // Assign the passed properties to the defined bank account fields above
+            Owner = name;
+            Balance = initalBalance;
+
+        }
+
+
+
+
+        // Action - these are the methods
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+    }
+}
